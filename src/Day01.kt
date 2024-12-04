e1d884373b98a76c1749562f25deedd9255ee315
Ties van de Ven <ties@tiesvandeven.nl>
2024-12-04 20:50:35 +0100

    day 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,32 @@
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
+    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
+        return input.map {
+            val split = it.split("   ")
+            Pair(split[0].toInt(), split[1].toInt())
+        }.fold(Pair(listOf(), listOf())) { acc, e ->
+            Pair(acc.first.plus(e.first), acc.second.plus(e.second))
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val parseInput = parseInput(input)
+        return parseInput.first.sorted().zip(parseInput.second.sorted())
+            .sumOf { max(it.first, it.second) - min(it.first, it.second) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val parseInput = parseInput(input)
+        val second = parseInput.second.groupBy { it }
+        return parseInput.first.sumOf { second.getOrElse(it) { listOf() }.size * it }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
---
+++ src/Day02.kt
@@ -0,0 +1,43 @@
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+    fun parseInput(input: List<String>): List<List<Int>> = input.map {
+        it.split(" ").map { it.toInt() }
+    }
+
+    fun part1(input: List<String>): Int {
+        val parseInput = parseInput(input)
+        return parseInput.filter { a ->
+            a.windowed(2).all {
+                val difference = max(it[0], it[1]) - min(it[0], it[1])
+                difference in 1..3
+            }
+        }.filter {
+            it.sorted() == it || it.sortedDescending() == it
+        }.size
+    }
+
+    fun part2(input: List<String>): Int = parseInput(input)
+        .map { a ->
+            // Map to a list of lists containing a list with 1 number missing for each position
+            List(a.size) { i -> a.filterIndexed { i2, _ -> i != i2 } }
+        }.filter { b: List<List<Int>> ->
+            b.any { c: List<Int> ->
+                c.windowed(2).all { d ->
+                    val difference = max(d[0], d[1]) - min(d[0], d[1])
+                    difference in 1..3
+                } && (c.sorted() == c || c.sortedDescending() == c)
+            }
+        }.size
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
